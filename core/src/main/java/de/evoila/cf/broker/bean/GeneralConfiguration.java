26bcc1e9a13280c837208c424abbf9e56c2f2e95
Johannes Hiemer <jhiemer@evoila.de>
2018-05-15 16:46:27 +0200

    Changing from @Service to @Configuration as described in docs for config beans

--- core/src/main/java/de/evoila/cf/broker/bean/GeneralConfiguration.java
+++ core/src/main/java/de/evoila/cf/broker/bean/GeneralConfiguration.java
@@ -1,10 +1,10 @@
 package de.evoila.cf.broker.bean;
 
 import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.stereotype.Service;
+import org.springframework.context.annotation.Configuration;
 
 /** @author Johannes Hiemer. */
-@Service
+@Configuration
 @ConfigurationProperties(prefix="general")
 public class GeneralConfiguration {
 
