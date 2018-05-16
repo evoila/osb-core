f19bced382c106a261a8f5357a363757cb4fe12f
René Schollmeyer <rschollmeyer@evoila.de>
2018-05-16 14:06:02 +0200

    fix(catalog): fix ServiceDefinitionDoesNotExist bug

--- core/src/main/java/de/evoila/cf/broker/controller/CatalogController.java
+++ core/src/main/java/de/evoila/cf/broker/controller/CatalogController.java
@@ -1,100 +1,29 @@
 package de.evoila.cf.broker.controller;
 
-import de.evoila.cf.broker.model.ServiceDefinition;
-import de.evoila.cf.broker.model.ServiceInstance;
+import de.evoila.cf.broker.model.Catalog;
+import de.evoila.cf.broker.service.CatalogService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.*;
-import de.evoila.cf.broker.model.Catalog;
-import de.evoila.cf.broker.service.CatalogService;
-
-import java.net.MalformedURLException;
-import java.net.URI;
-import java.net.URISyntaxException;
-import java.net.URL;
-import java.util.Arrays;
-import java.util.stream.Collectors;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 /** @author Johannes Hiemer. */
 @Controller
 @RequestMapping(value = "/v2/catalog")
 public class CatalogController extends BaseController {
 
-    public static final String TEST_PROFILE = "test";
     private final Logger logger = LoggerFactory.getLogger(CatalogController.class);
 
 	@Autowired
 	private CatalogService service;
 
-	@Autowired
-    private Environment environment;
-
 	@GetMapping(value = { "/", "" })
 	public @ResponseBody Catalog getCatalog(){
 		logger.debug("GET: getCatalog()");
-		return prepareCatalogIfTesting(service.getCatalog());
+		return service.getCatalog();
 	}
 
-	private Catalog prepareCatalogIfTesting(Catalog catalog) {
-        if (Arrays.stream(environment.getActiveProfiles()).anyMatch(
-                env -> (env.equalsIgnoreCase(TEST_PROFILE)))) {
-
-            catalog.getServices().stream().map(service -> {
-                if (service.getName().indexOf(TEST_PROFILE) == -1)
-                    service.setName(service.getName() + "-" + TEST_PROFILE);
-
-                service.setId(replaceLastChar(service.getId()));
-                service.getDashboardClient()
-                        .setSecret(replaceLastChar(service.getDashboardClient().getSecret()));
-
-
-                if (service.getDashboardClient().getId().indexOf(TEST_PROFILE) == -1)
-                    service.getDashboardClient().setId(
-                            service.getDashboardClient().getId() + "-" + TEST_PROFILE
-                    );
-
-                service.getDashboard().setUrl(
-                        replaceUrl(service.getDashboard().getUrl())
-                );
-                service.getDashboardClient().setRedirectUri(
-                        replaceUrl(service.getDashboardClient().getRedirectUri())
-                );
-
-                service.getPlans().stream().map(plan -> {
-                    plan.setId(replaceLastChar(plan.getId()));
-
-                    return plan;
-                }).collect(Collectors.toList());
-                return service;
-            }).collect(Collectors.toList());
-        }
-        return catalog;
-    }
-
-    private String replaceLastChar(String value) {
-	    if (value != null && value.length() > 1)
-	        return value.substring(0, value.length() - 1).concat("T");
-
-        return value;
-    }
-
-    private String replaceUrl(String urlStr) {
-	    try {
-            URL url = new URL(urlStr);
-
-            if (url.getHost().indexOf(TEST_PROFILE) == -1) {
-                URL newURL = new URL(url.getProtocol(),
-                        url.getHost().replaceFirst("\\.", "-" + TEST_PROFILE + "."),
-                        url.getPort(), url.getFile());
-                urlStr = newURL.toString();
-            }
-        } catch(MalformedURLException ex) {
-            logger.info("Exception replacing URL", ex);
-        }
-
-        return urlStr;
-    }
 }
