14a7c1a74b4249b5e44de20d1ba525ce1976f721
Johannes Hiemer <jhiemer@evoila.de>
2018-07-26 08:48:23 +0200

    Changing layout of HaProxyConfiguration

--- core/src/main/java/de/evoila/cf/broker/bean/HAProxyConfiguration.java
+++ core/src/main/java/de/evoila/cf/broker/bean/HAProxyConfiguration.java
@@ -14,21 +14,16 @@ import org.springframework.context.annotation.Configuration;
 public class HAProxyConfiguration {
 
 	private String uri;
-	
-	private Auth auth;
-	
-	public static class Auth {
 
-		private String token;
+    private String token;
 
-		public String getToken() {
-			return token;
-		}
+    public String getToken() {
+        return token;
+    }
 
-		public void setToken(String token) {
-			this.token = token;
-		}
-	}
+    public void setToken(String token) {
+        this.token = token;
+    }
 
 	public String getUri() {
 		return uri;
@@ -37,16 +32,5 @@ public class HAProxyConfiguration {
 	public void setUri(String uri) {
 		this.uri = uri;
 	}
-	
-	public Auth getAuth() {
-		return auth;
-	}
 
-	public void setAuth(Auth auth) {
-		this.auth = auth;
-	}
-
-	public String getAuthToken() {
-	    return auth.getToken();
-	}
 }
