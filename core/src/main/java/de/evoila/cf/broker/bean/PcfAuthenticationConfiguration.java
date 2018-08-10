ff0519939c69f5dccc7ba9c49af3bf73ebca07bb
Yannic Remmet <yremmet@evilgeniuslab.net>
2018-08-10 17:45:19 +0200

    fix(security) fixes pcfAuthenticationConfig

--- core/src/main/java/de/evoila/cf/broker/bean/PcfAuthenticationConfiguration.java
+++ core/src/main/java/de/evoila/cf/broker/bean/PcfAuthenticationConfiguration.java
@@ -18,4 +18,24 @@ public class PcfAuthenticationConfiguration extends BaseAuthenticationConfigurat
 
     @Value("${security.user.password}")
     private String password;
+
+    @Override
+    public String getUsername() {
+        return username;
+    }
+
+    @Override
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    @Override
+    public String getPassword() {
+        return password;
+    }
+
+    @Override
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
