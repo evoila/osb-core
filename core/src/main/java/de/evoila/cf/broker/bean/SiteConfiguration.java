cfdf11d886d755aee21a8324310fc1f1ba958d59
René Schollmeyer <rschollmeyer@evoila.de>
2018-09-14 13:58:18 +0200

    fix: removed conditionalonproperty because spring cant handle maps or lists

--- core/src/main/java/de/evoila/cf/broker/bean/SiteConfiguration.java
+++ core/src/main/java/de/evoila/cf/broker/bean/SiteConfiguration.java
@@ -1,15 +1,15 @@
 package de.evoila.cf.broker.bean;
 
-import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
 
 import java.util.Map;
 
+@Configuration
 @ConfigurationProperties(prefix = "site")
-@ConditionalOnProperty(prefix = "site", name = "properties", havingValue = "")
 public class SiteConfiguration {
 
-    Map<String, Object> properties;
+    private Map<String, Object> properties;
 
     public Map<String, Object> getProperties() {
         return properties;
