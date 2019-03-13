b7716f82e9cadbdf5801831b29fe7b034f41cb2b
Patrick Weber <pweber@evoila.de>
2019-03-13 11:36:47 +0100

    add missing files

---
+++ core/src/main/java/de/evoila/cf/broker/custom/PcfSchema.java
@@ -0,0 +1,61 @@
+package de.evoila.cf.broker.custom;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import de.evoila.cf.broker.bean.EndpointConfiguration;
+import de.evoila.cf.broker.interfaces.TranformCatalog;
+import de.evoila.cf.broker.model.catalog.Catalog;
+import de.evoila.cf.broker.model.catalog.plan.Plan;
+import de.evoila.cf.broker.model.catalog.plan.Schemas;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Profile;
+import org.springframework.core.annotation.Order;
+import org.springframework.core.env.Environment;
+import org.springframework.stereotype.Component;
+
+import java.io.IOException;
+import java.util.Map;
+
+
+/**
+ * @author Patrick Weber
+ */
+
+@Component
+@Profile("pcf")
+@Order(20)
+public class PcfSchema implements TranformCatalog {
+
+    private ObjectMapper objectMapper;
+
+    PcfSchema(ObjectMapper objectMapper){
+        this.objectMapper = objectMapper;
+    }
+
+    @Override
+    public void tranform(Catalog catalog, Environment environment, EndpointConfiguration endpointConfiguration) {
+        catalog.getServices().forEach(s -> s.getPlans().forEach(this::convert));
+
+    }
+
+    @Override
+    public void clean(Catalog catalog, Environment environment, EndpointConfiguration endpointConfiguration) {
+        catalog.getServices().forEach(s -> s.getPlans().forEach(this::cleanJson));
+    }
+
+    private void convert(Plan plan){
+        Map<String, Object> customParameter = plan.getMetadata().getCustomParameters();
+        if (customParameter.containsKey("schemajson")) {
+            String tsParam = (String) customParameter.get("schemajson");
+            try {
+                plan.setSchemas(objectMapper.readValue(tsParam, Schemas.class));
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    private void cleanJson(Plan plan){
+        Map<String, Object> customParameter = plan.getMetadata().getCustomParameters();
+        customParameter.remove("schemajson");
+    }
+}
---
+++ core/src/main/java/de/evoila/cf/broker/interfaces/TranformCatalog.java
@@ -0,0 +1,15 @@
+package de.evoila.cf.broker.interfaces;
+
+import de.evoila.cf.broker.bean.EndpointConfiguration;
+import org.springframework.core.env.Environment;
+import de.evoila.cf.broker.model.catalog.Catalog;
+
+
+/**
+ * @author Patrick Weber
+ */
+
+public interface TranformCatalog {
+    void tranform(Catalog catalog, Environment environment, EndpointConfiguration endpointConfiguration);
+    void clean(Catalog catalog, Environment environment, EndpointConfiguration endpointConfiguration);
+}
