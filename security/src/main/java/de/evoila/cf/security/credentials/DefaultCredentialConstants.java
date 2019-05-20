aaea811ad13610fca6c04e4a4702c0f456e8ba5f
Johannes Hiemer <jhiemer@evoila.de>
2019-05-20 07:35:06 +0200

    chore(): adding DefaultCredentialConstants for OSB-849/OSB-850

---
+++ security/src/main/java/de/evoila/cf/security/credentials/DefaultCredentialConstants.java
@@ -0,0 +1,11 @@
+package de.evoila.cf.security.credentials;
+
+/**
+ * @author Johannes Hiemer.
+ */
+public class DefaultCredentialConstants {
+
+    public static String BACKUP_CREDENTIALS = "backup_credentials";
+
+    public static String BACKUP_AGENT_CREDENTIALS = "backup_agent_credentials";
+}
