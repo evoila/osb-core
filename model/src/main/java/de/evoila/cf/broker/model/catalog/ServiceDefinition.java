431e6ed555ce3ddd344c603682fcd94763850f2d
Johannes Strauß <jstrauss@evoila.de>
2019-08-08 16:33:16 +0200

    feat(): maintenance_info to catalog

--- model/src/main/java/de/evoila/cf/broker/model/catalog/ServiceDefinition.java
+++ model/src/main/java/de/evoila/cf/broker/model/catalog/ServiceDefinition.java
@@ -10,7 +10,7 @@ import java.util.List;
 
 /**
  * A service offered by this broker.
- * 
+ *
  * @author sgreenberg@gopivotal.com
  * @author Johannes Hiemer.
  * @author Marco Di Martino.
@@ -18,174 +18,184 @@ import java.util.List;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class ServiceDefinition {
 
-	private String id;
+    private String id;
 
-	private String name;
+    private String name;
 
-	private String description;
+    private String description;
 
-	private boolean bindable;
+    private boolean bindable;
 
-	private List<Plan> plans = new ArrayList<>();
+    private List<Plan> plans = new ArrayList<>();
 
-	private List<String> tags = new ArrayList<>();
+    private List<String> tags = new ArrayList<>();
 
-	private ServiceMetadata metadata;
+    private ServiceMetadata metadata;
 
-	private List<String> requires = new ArrayList<>();
+    private List<String> requires = new ArrayList<>();
 
-	private Dashboard dashboard;
+    private Dashboard dashboard;
 
-	@JsonProperty("instances_retrievable")
-	private boolean instancesRetrievable;
+    @JsonProperty("instances_retrievable")
+    private boolean instancesRetrievable;
 
-	@JsonProperty("bindings_retrievable")
-	private boolean bindingsRetrievable;
+    @JsonProperty("bindings_retrievable")
+    private boolean bindingsRetrievable;
 
-	@JsonProperty("dashboard_client")
-	private DashboardClient dashboardClient;
+    @JsonProperty("dashboard_client")
+    private DashboardClient dashboardClient;
 
-	@JsonProperty("plan_updateable") // misspelling of attribute kept, do not change it
-	private boolean updateable;
+    @JsonProperty("plan_updateable") // misspelling of attribute kept, do not change it
+    private boolean updateable;
 
-	public ServiceDefinition() {
-	}
+    @JsonProperty("maintenance_info")
+    private MaintenanceInfo maintenanceInfo;
 
-	public ServiceDefinition(String id, String name, String description, boolean bindable, List<Plan> plans, boolean updatable) {
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.bindable = bindable;
-		this.setPlans(plans);
-		this.updateable = updatable;
-	}
+    public ServiceDefinition() {
+    }
 
-	public ServiceDefinition(String id, String name, String description, boolean bindable, List<Plan> plans, boolean updatable, List<String> requires) {
-		this.id = id;
-		this.name = name;
-		this.description = description;
-		this.bindable = bindable;
-		this.updateable = updatable;
-		this.setPlans(plans);
-		this.setRequires(requires);
-	}
+    public ServiceDefinition(String id, String name, String description, boolean bindable, List<Plan> plans, boolean updatable) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.bindable = bindable;
+        this.setPlans(plans);
+        this.updateable = updatable;
+    }
 
-	public ServiceDefinition(String id, String name, String description, boolean bindable, List<Plan> plans, boolean updatable,
-			List<String> tags, ServiceMetadata metadata, List<String> requires, boolean instancesRetrievable, boolean bindingsRetrievable) {
-		this(id, name, description, bindable, plans, updatable);
-		setTags(tags);
-		setMetadata(metadata);
-		setRequires(requires);
-		setInstancesRetrievable(instancesRetrievable);
-		setBindingsRetrievable(bindingsRetrievable);
-	}
+    public ServiceDefinition(String id, String name, String description, boolean bindable, List<Plan> plans, boolean updatable, List<String> requires) {
+        this.id = id;
+        this.name = name;
+        this.description = description;
+        this.bindable = bindable;
+        this.updateable = updatable;
+        this.setPlans(plans);
+        this.setRequires(requires);
+    }
 
-	public ServiceDefinition(String id, String name, String description, boolean bindable, List<Plan> plans) {
-		this(id, name, description, bindable, plans, false);
-	}
+    public ServiceDefinition(String id, String name, String description, boolean bindable, List<Plan> plans, boolean updatable,
+                             List<String> tags, ServiceMetadata metadata, List<String> requires, boolean instancesRetrievable, boolean bindingsRetrievable) {
+        this(id, name, description, bindable, plans, updatable);
+        setTags(tags);
+        setMetadata(metadata);
+        setRequires(requires);
+        setInstancesRetrievable(instancesRetrievable);
+        setBindingsRetrievable(bindingsRetrievable);
+    }
 
-	public boolean isBindingsRetrievable() {
-		return bindingsRetrievable;
-	}
+    public ServiceDefinition(String id, String name, String description, boolean bindable, List<Plan> plans) {
+        this(id, name, description, bindable, plans, false);
+    }
 
-	public void setBindingsRetrievable(boolean bindingsRetrievable) {
-		this.bindingsRetrievable = bindingsRetrievable;
-	}
+    public boolean isBindingsRetrievable() {
+        return bindingsRetrievable;
+    }
 
-	public String getId() {
-		return id;
-	}
+    public void setBindingsRetrievable(boolean bindingsRetrievable) {
+        this.bindingsRetrievable = bindingsRetrievable;
+    }
 
-	public void setId(String id) {
-		this.id = id;
-	}
+    public String getId() {
+        return id;
+    }
 
-	public String getName() {
-		return name;
-	}
+    public void setId(String id) {
+        this.id = id;
+    }
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public String getDescription() {
-		return description;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	public void setDescription(String description) {
-		this.description = description;
-	}
+    public String getDescription() {
+        return description;
+    }
 
-	public boolean isBindable() {
-		return bindable;
-	}
+    public void setDescription(String description) {
+        this.description = description;
+    }
 
-	public void setBindable(boolean bindable) {
-		this.bindable = bindable;
-	}
+    public boolean isBindable() {
+        return bindable;
+    }
 
-	public boolean isUpdateable() {
-		return updateable;
-	}
+    public void setBindable(boolean bindable) {
+        this.bindable = bindable;
+    }
 
-	public void setPlanUpdateable(boolean updatable) {
-		this.updateable = updatable;
-	}
+    public boolean isUpdateable() {
+        return updateable;
+    }
 
-	public List<Plan> getPlans() {
-		return plans;
-	}
+    public void setPlanUpdateable(boolean updatable) {
+        this.updateable = updatable;
+    }
 
-	public void setPlans(List<Plan> plans) {
-		this.plans = plans;
-	}
+    public List<Plan> getPlans() {
+        return plans;
+    }
 
-	public List<String> getTags() {
-		return tags;
-	}
+    public void setPlans(List<Plan> plans) {
+        this.plans = plans;
+    }
 
-	public void setTags(List<String> tags) {
-		this.tags = tags;
-	}
+    public List<String> getTags() {
+        return tags;
+    }
 
-	public List<String> getRequires() {
-		return requires;
-	}
+    public void setTags(List<String> tags) {
+        this.tags = tags;
+    }
 
-	public void setRequires(List<String> requires) {
-		this.requires = requires;
-	}
+    public List<String> getRequires() {
+        return requires;
+    }
 
-	public ServiceMetadata getMetadata() {
-		return metadata;
-	}
+    public void setRequires(List<String> requires) {
+        this.requires = requires;
+    }
 
-	public void setMetadata(ServiceMetadata metadata) {
-	    this.metadata = metadata;
-	}
-	
-	public Dashboard getDashboard() {
-		return dashboard;
-	}
+    public ServiceMetadata getMetadata() {
+        return metadata;
+    }
 
-	public void setDashboard(Dashboard dashboard) {
-		this.dashboard = dashboard;
-	}
+    public void setMetadata(ServiceMetadata metadata) {
+        this.metadata = metadata;
+    }
 
-	public DashboardClient getDashboardClient() {
-		return dashboardClient;
-	}
+    public Dashboard getDashboard() {
+        return dashboard;
+    }
 
-	public void setDashboardClient(DashboardClient dashboardClient) {
-		this.dashboardClient = dashboardClient;
-	}
+    public void setDashboard(Dashboard dashboard) {
+        this.dashboard = dashboard;
+    }
 
-	public boolean isInstancesRetrievable() {
-		return instancesRetrievable;
-	}
+    public DashboardClient getDashboardClient() {
+        return dashboardClient;
+    }
 
-	public void setInstancesRetrievable(boolean instancesRetrievable) {
-		this.instancesRetrievable = instancesRetrievable;
-	}
+    public void setDashboardClient(DashboardClient dashboardClient) {
+        this.dashboardClient = dashboardClient;
+    }
 
+    public boolean isInstancesRetrievable() {
+        return instancesRetrievable;
+    }
+
+    public void setInstancesRetrievable(boolean instancesRetrievable) {
+        this.instancesRetrievable = instancesRetrievable;
+    }
+
+    public MaintenanceInfo getMaintenanceInfo() {
+        return maintenanceInfo;
+    }
+
+    public void setMaintenanceInfo(MaintenanceInfo maintenanceInfo) {
+        this.maintenanceInfo = maintenanceInfo;
+    }
 }
