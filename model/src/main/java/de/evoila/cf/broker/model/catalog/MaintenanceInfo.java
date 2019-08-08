ba91786f04afc1f938e229d313cbf62fd1aa0c31
Johannes Strauß <jstrauss@evoila.de>
2019-08-08 14:09:28 +0200

    feat(): add maintenance_info to catalog

---
+++ model/src/main/java/de/evoila/cf/broker/model/catalog/MaintenanceInfo.java
@@ -0,0 +1,31 @@
+package de.evoila.cf.broker.model.catalog;
+
+public class MaintenanceInfo {
+
+    private String version;
+    private String description;
+
+    public MaintenanceInfo() {
+    }
+
+    public MaintenanceInfo(String version, String description) {
+        this.version = version;
+        this.description = description;
+    }
+
+    public String getVersion() {
+        return version;
+    }
+
+    public void setVersion(String version) {
+        this.version = version;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
