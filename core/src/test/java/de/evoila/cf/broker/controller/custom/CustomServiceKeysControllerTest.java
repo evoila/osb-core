92f312101f3fcb4a970df3fb286fbd435ae124ec
Manuel Waldner <43695452+manuelwaldner@users.noreply.github.com>
2019-10-17 13:17:30 +0200

    test(CustomServiceKeysController): Add tests for `getServiceKey` method

--- core/src/test/java/de/evoila/cf/broker/controller/custom/CustomServiceKeysControllerTest.java
+++ core/src/test/java/de/evoila/cf/broker/controller/custom/CustomServiceKeysControllerTest.java
@@ -21,12 +21,14 @@ import de.evoila.cf.broker.repository.ServiceInstanceRepository;
 import de.evoila.cf.broker.service.BindingService;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertSame;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
 class CustomServiceKeysControllerTest {
 
-    private static final String HAPPY_SERVICE_INSTANCE_ID = "2b2312d2-c964-4e3d-9d0c-f8e6543f76fb";
+    private static final String HAPPY_SERVICE_INSTANCE_ID   = "2b2312d2-c964-4e3d-9d0c-f8e6543f76fb";
+    private static final String HAPPY_BINDING_ID            = "2357f8a8-3c5c-4f06-8d90-f8493d8177b2";
 
     @Mock
     private BindingRepository bindingRepository;
@@ -34,6 +36,9 @@ class CustomServiceKeysControllerTest {
     private BindingService bindingService;
     @Mock
     private ServiceInstanceRepository serviceInstanceRepository;
+
+    @Mock
+    private ServiceInstanceBinding serviceInstanceBinding;
 
     private CustomServiceKeysController controller;
 
@@ -85,6 +90,34 @@ class CustomServiceKeysControllerTest {
 
     @Nested
     class getServiceKeyMethod {
+
+        @Nested
+        class okResponse {
+
+            private void validateResponse(ResponseEntity<ServiceInstanceBinding> response) {
+                assertEquals(HttpStatus.OK, response.getStatusCode());
+                assertSame(serviceInstanceBinding, response.getBody());
+            }
+
+            @Test
+            void withNullServiceInstanceId() {
+                when(bindingRepository.findOne(HAPPY_BINDING_ID))
+                        .thenReturn(serviceInstanceBinding);
+                ResponseEntity<ServiceInstanceBinding> response = controller.getServiceKey(null,
+                                                                                           HAPPY_BINDING_ID);
+                validateResponse(response);
+            }
+
+            @Test
+            void withServiceInstanceId() {
+                when(bindingRepository.findOne(HAPPY_BINDING_ID))
+                        .thenReturn(serviceInstanceBinding);
+                ResponseEntity<ServiceInstanceBinding> response = controller.getServiceKey(HAPPY_SERVICE_INSTANCE_ID,
+                                                                                           HAPPY_BINDING_ID);
+                validateResponse(response);
+            }
+
+        }
 
     }
 
