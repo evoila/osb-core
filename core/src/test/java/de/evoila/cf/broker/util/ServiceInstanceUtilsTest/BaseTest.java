b625de7e93ccb7f2094ede2c0af8f8c8f39408b5
Manuel Waldner <43695452+manuelwaldner@users.noreply.github.com>
2019-10-23 15:10:29 +0200

    test(ServiceInstanceUtils): Add tests for `wouldCreateIdenticalInstance` method

--- core/src/test/java/de/evoila/cf/broker/util/ServiceInstanceUtilsTest/BaseTest.java
+++ core/src/test/java/de/evoila/cf/broker/util/ServiceInstanceUtilsTest/BaseTest.java
@@ -1,7 +1,12 @@
 package de.evoila.cf.broker.util.ServiceInstanceUtilsTest;
 
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+
+import de.evoila.cf.broker.model.ServiceInstance;
+import de.evoila.cf.broker.model.ServiceInstanceRequest;
+import de.evoila.cf.broker.model.context.Context;
 
 @ExtendWith(MockitoExtension.class)
 abstract class BaseTest {
@@ -11,5 +16,18 @@ abstract class BaseTest {
     static final String HOSTNAME = "hostname";
     static final String PORT = "port";
     static final String HOSTS = "hosts";
+
+    static final String HAPPY_SERVICE_INSTANCE_ID   = "57817290-9c56-453d-8961-eb4f80c427a6";
+    static final String HAPPY_SERVICE_DEFINITION_ID = "83927042-5573-41dc-ae51-12e13f9ee42f";
+    static final String HAPPY_PLAN_ID               = "d0851f8d-c201-4e11-8453-7dbbcdaf8ec3";
+    static final String HAPPY_ORGANIZATION_GUID     = "ac80b58e-5c29-4f1c-897c-b2c36326ff0f";
+    static final String HAPPY_SPACE_GUID            = "4ec8d1c7-bba0-4da1-ad70-8ac2d0f0eb44";
+
+    @Mock
+    ServiceInstanceRequest request;
+    @Mock
+    ServiceInstance serviceInstance;
+    @Mock
+    Context context;
 
 }
