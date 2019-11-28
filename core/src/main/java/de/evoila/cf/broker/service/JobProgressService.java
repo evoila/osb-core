842aa15e15435d68422e890e1cdc7e1138ce0aac
Johannes Strauß <jstrauss@evoila.de>
2019-11-28 10:52:50 +0100

    fix(JobProgressService): exceptionHandling
    handle null pointer exception here to unify behavior and reduce line of code.

--- core/src/main/java/de/evoila/cf/broker/service/JobProgressService.java
+++ core/src/main/java/de/evoila/cf/broker/service/JobProgressService.java
@@ -1,14 +1,13 @@
-/**
- * 
- */
+
 package de.evoila.cf.broker.service;
 
+import de.evoila.cf.broker.exception.ServiceBrokerException;
 import de.evoila.cf.broker.model.JobProgress;
 import de.evoila.cf.broker.repository.JobRepository;
 import org.springframework.stereotype.Service;
 
 /**
- * @author Christian Brinker, Marco Di Martino, Johannes Hiemer.
+ * @author Christian Brinker, Marco Di Martino, Johannes Hiemer, Johannes Strauß.
  */
 @Service
 public class JobProgressService  {
@@ -27,9 +26,15 @@ public class JobProgressService  {
         return jobRepository.getJobProgressByReferenceId(referenceId);
     }
 
-	public JobProgress startJob(String id, String referenceId, String description, String operation) {
-		return jobRepository.saveJobProgress(id, referenceId, JobProgress.IN_PROGRESS, description, operation);
-	}
+	public JobProgress startJob(String id, String referenceId, String description, String operation) throws ServiceBrokerException {
+        JobProgress jobProgress = jobRepository.saveJobProgress(id, referenceId, JobProgress.IN_PROGRESS, description, operation);
+
+        if (jobProgress == null) {
+            throw new ServiceBrokerException("An Error occurred while saving new JobProgress object with Id: " + id);
+        }
+
+        return jobProgress;
+    }
 
 	public JobProgress failJob(String id, String description) {
         return jobRepository.updateJobProgress(id, JobProgress.FAILED, description);
