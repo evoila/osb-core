4463726c43ea607afd08dc03b49e729c5aeae7a1
Johannes Strauß <jstrauss@evoila.de>
2019-11-28 18:20:10 +0100

    fix(AsyncCreateInstanceTest): fix test according to new behaviour
    add new tests for new exception handling, and fix the old ones.

--- core/src/test/java/de/evoila/cf/broker/service/impl/AsyncDeploymentServiceImplTest/AsyncCreateInstanceTest.java
+++ core/src/test/java/de/evoila/cf/broker/service/impl/AsyncDeploymentServiceImplTest/AsyncCreateInstanceTest.java
@@ -2,6 +2,7 @@ package de.evoila.cf.broker.service.impl.AsyncDeploymentServiceImplTest;
 
 import de.evoila.cf.broker.exception.ServiceBrokerException;
 import de.evoila.cf.broker.model.JobProgress;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import static org.mockito.ArgumentMatchers.anyString;
@@ -10,20 +11,24 @@ import static org.mockito.Mockito.when;
 
 class AsyncCreateInstanceTest extends BaseTest {
 
-    private void whensForSyncCreateInstanceThrowsException(JobProgress returnOfFailJob) throws ServiceBrokerException
-    {
+    private static final String JOB_PROGRESS_DESCRIPTION = "Creating service..";
+
+
+    private void createInstanceThrowsException(Exception expectedException) throws ServiceBrokerException{
         mockSuccessfulStartJob(JobProgress.PROVISION);
-        ServiceBrokerException expectedException = new ServiceBrokerException("Test");
         when(deploymentService.syncCreateInstance(serviceInstance, parameters, plan, platformService))
                 .thenThrow(expectedException);
-        when(startedJob.getId())
-                .thenReturn(JOB_PROGRESS_ID);
-        when(jobProgressService.failJob(eq(JOB_PROGRESS_ID), anyString()))
-                .thenReturn(returnOfFailJob);
     }
 
-    private void whensForSyncCreateInstanceSucceeds(JobProgress returnOfSucceedProgress) throws ServiceBrokerException
-    {
+    private void whensForSyncCreateInstanceThrowsServiceBrokerException() throws ServiceBrokerException {
+        createInstanceThrowsException(new ServiceBrokerException("Test"));
+    }
+
+    private void whensForSyncCreateInstanceThrowsRuntimeException() throws ServiceBrokerException {
+        createInstanceThrowsException(new RuntimeException("Test"));
+    }
+
+    private void whensForSyncCreateInstanceSucceeds(JobProgress returnOfSucceedProgress) throws ServiceBrokerException {
         mockSuccessfulStartJob(JobProgress.PROVISION);
         when(deploymentService.syncCreateInstance(serviceInstance, parameters, plan, platformService))
                 .thenReturn(serviceInstance);
@@ -34,42 +39,47 @@ class AsyncCreateInstanceTest extends BaseTest {
     }
 
     @Test
-    void startJobReturnsNull()
-    {
-        when(serviceInstance.getId())
-                .thenReturn(SERVICE_INSTANCE_ID);
-        when(jobProgressService.startJob(eq(JOB_PROGRESS_ID), eq(SERVICE_INSTANCE_ID), anyString(), eq(JobProgress.PROVISION)))
-                .thenReturn(null);
+    @DisplayName("Should log exception and update JobProgress object, when startJob(...) throws runtime exception.")
+    void startJobThrowsRuntimeException() throws ServiceBrokerException {
+        mockStartJobThrowsRuntimeException(JobProgress.PROVISION, JOB_PROGRESS_DESCRIPTION);
+        asyncDeploymentService.asyncCreateInstance(deploymentService, serviceInstance, parameters, plan, platformService, JOB_PROGRESS_ID);
 
-        asyncDeploymentService.asyncCreateInstance(null, serviceInstance, null, null, null, JOB_PROGRESS_ID);
     }
 
     @Test
-    void syncCreateInstanceThrowsExceptionFailJobReturnsNull() throws ServiceBrokerException {
-        whensForSyncCreateInstanceThrowsException(null);
+    @DisplayName("Should log exception, when startJob(...) fails with ServiceBrokerException.")
+    void startJobThrowsServiceBrokerException() throws ServiceBrokerException {
+        mockStartJobThrowsServiceBrokerException(JobProgress.PROVISION, JOB_PROGRESS_DESCRIPTION);
 
         asyncDeploymentService.asyncCreateInstance(deploymentService, serviceInstance, parameters, plan, platformService, JOB_PROGRESS_ID);
     }
 
     @Test
-    void syncCreateInstanceThrowsExceptionFailJobReturnsObject() throws ServiceBrokerException
-    {
-        whensForSyncCreateInstanceThrowsException(completedJob);
+    @DisplayName("Should log exception, when syncCreateInstance(...) fails with ServiceBrokerException.")
+    void syncCreateInstanceThrowsServiceBrokerException() throws ServiceBrokerException {
+        whensForSyncCreateInstanceThrowsServiceBrokerException();
 
         asyncDeploymentService.asyncCreateInstance(deploymentService, serviceInstance, parameters, plan, platformService, JOB_PROGRESS_ID);
     }
 
     @Test
-    void syncCreateInstanceSucceedsReturnsNull() throws ServiceBrokerException
-    {
+    @DisplayName("Should log exception and update JobProgress object, when syncCreateInstance(...) fails with RuntimeException.")
+    void syncCreateInstanceThrowsRuntimeException() throws ServiceBrokerException {
+        whensForSyncCreateInstanceThrowsRuntimeException();
+
+        asyncDeploymentService.asyncCreateInstance(deploymentService, serviceInstance, parameters, plan, platformService, JOB_PROGRESS_ID);
+    }
+
+    @Test
+    void syncCreateInstanceSucceedsReturnsNull() throws ServiceBrokerException {
         whensForSyncCreateInstanceSucceeds(null);
 
         asyncDeploymentService.asyncCreateInstance(deploymentService, serviceInstance, parameters, plan, platformService, JOB_PROGRESS_ID);
     }
 
     @Test
-    void syncCreateInstanceSucceedsReturnsObject() throws ServiceBrokerException
-    {
+    @DisplayName("Should not fail, when no exceptions occurs.")
+    void syncCreateInstanceSucceedsReturnsObject() throws ServiceBrokerException {
         whensForSyncCreateInstanceSucceeds(completedJob);
 
         asyncDeploymentService.asyncCreateInstance(deploymentService, serviceInstance, parameters, plan, platformService, JOB_PROGRESS_ID);
