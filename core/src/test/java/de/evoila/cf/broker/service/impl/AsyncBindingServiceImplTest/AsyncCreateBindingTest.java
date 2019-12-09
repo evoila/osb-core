fb9ec00980e286ff4a18e1efdfe927641038e4ad
Johannes Strauß <jstrauss@evoila.de>
2019-12-09 15:32:15 +0100

    feat(AsyncBindingServiceImplTest): new tests for async binding
    Fix old tests according to new behavior.

---
+++ core/src/test/java/de/evoila/cf/broker/service/impl/AsyncBindingServiceImplTest/AsyncCreateBindingTest.java
@@ -0,0 +1,74 @@
+package de.evoila.cf.broker.service.impl.AsyncBindingServiceImplTest;
+
+import de.evoila.cf.broker.exception.*;
+import de.evoila.cf.broker.model.JobProgress;
+import de.evoila.cf.broker.model.ServiceInstanceBindingRequest;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
+
+import static org.mockito.Mockito.*;
+
+class AsyncCreateBindingTest extends BaseTest {
+
+    @Mock
+    ServiceInstanceBindingRequest serviceInstanceBindingRequest;
+
+    private final static String DESCRIPTION = "Creating binding..";
+
+    @Test
+    @DisplayName("Should log exception, when jobStart(...) throws ServiceBroker Exception")
+    void startJobThrowsServiceBrokerException() throws ServiceBrokerException {
+        ServiceBrokerException serviceBrokerException = new ServiceBrokerException("Test");
+        mockStartJobThrowsException(JobProgress.BIND, DESCRIPTION, serviceBrokerException);
+        runCreateBinding();
+    }
+
+    @Test
+    @DisplayName("Should log exception, when jobStart(...), throws RuntimeException")
+    void startJobThrowsRuntimeException() throws ServiceBrokerException {
+        RuntimeException runtimeException = new RuntimeException("Test");
+        mockStartJobThrowsException(JobProgress.BIND, DESCRIPTION, runtimeException);
+        runCreateBinding();
+        verify(jobProgressService, times(1))
+                .failJob(JOB_PROGRESS_ID, "Internal error during instance binding creation, please contact our support.");
+    }
+
+    @Test
+    @DisplayName("Should log exception, when createBinding(...) throws ServiceBrokerException")
+    void createBindingThrowsServiceBrokerException() throws ServiceBrokerException, InvalidParametersException, PlatformException {
+        mockStartJobReturnsJobProgress(JobProgress.BIND, DESCRIPTION);
+        when(bindingService.createBinding(SERVICE_BINDING_ID, serviceInstance, serviceInstanceBindingRequest, plan))
+                .thenThrow(new ServiceBrokerException("Test"));
+        runCreateBinding();
+    }
+
+    @Test
+    @DisplayName("Should log exception, when createBinding(...) throws RuntimeException")
+    void createBindingThrowsRuntimeException() throws ServiceBrokerException, InvalidParametersException, PlatformException {
+        mockStartJobReturnsJobProgress(JobProgress.BIND, DESCRIPTION);
+        when(bindingService.createBinding(SERVICE_BINDING_ID, serviceInstance, serviceInstanceBindingRequest, plan))
+                .thenThrow(new RuntimeException("Test"));
+        runCreateBinding();
+        verify(jobProgressService, times(1))
+                .failJob(JOB_PROGRESS_ID, "Internal error during instance binding creation, please contact our support.");
+    }
+
+
+    @Test
+    @DisplayName("Should not fail, when no exceptions occurs.")
+    void syncCreateBindingSucceedsReturnsObject() throws ServiceBrokerException, PlatformException, InvalidParametersException {
+        mockStartJobReturnsJobProgress(JobProgress.BIND, DESCRIPTION);
+        runCreateBinding();
+        verify(bindingService, times(1)).createBinding(
+                SERVICE_BINDING_ID, serviceInstance, serviceInstanceBindingRequest, plan);
+        verify(jobProgressService, times(1)).succeedProgress(JOB_PROGRESS_ID, "Instance Binding successfully created");
+    }
+
+    private void runCreateBinding() {
+        asyncBindingService.asyncCreateServiceInstanceBinding(bindingService,
+                SERVICE_BINDING_ID,
+                serviceInstance,
+                serviceInstanceBindingRequest, plan, true, JOB_PROGRESS_ID);
+    }
+}
