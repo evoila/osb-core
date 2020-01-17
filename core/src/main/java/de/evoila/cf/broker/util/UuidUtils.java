682df841b0e457519b2af25cc3fc2a5cd031c746
Johannes Strauß <jstrauss@evoila.de>
2020-01-17 12:00:09 +0100

    feat(UuidUtils): Add isValidUUid method
    Add Method to check if a given string is a valid UUID.

--- core/src/main/java/de/evoila/cf/broker/util/UuidUtils.java
+++ core/src/main/java/de/evoila/cf/broker/util/UuidUtils.java
@@ -1,5 +1,8 @@
 package de.evoila.cf.broker.util;
 
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
 public class UuidUtils {
 
     public static final String UUID_REGEX = "^[0-9A-Fa-f]{8}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{4}-[0-9A-Fa-f]{12}$";
@@ -7,4 +10,7 @@ public class UuidUtils {
     public static final String NOT_A_UUID_MESSAGE = "The ServiceInstanceID is not a valid UUID. Please only use valid UUIDs," +
             " too avoid unexpected errors.";
 
+    public static boolean isValidUUID(String uuid) {
+        return uuid != null && Pattern.matches(UUID_REGEX, uuid);
+    }
 }
