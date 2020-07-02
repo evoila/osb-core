9167ecd15863b3ca7eff21715659a616abf85778
Johannes Strauß <jstrauss@evoila.de>
2020-07-02 11:38:48 +0200

    fix(JobProgressUtils): retry-after default value
    use default values instead of null

--- core/src/main/java/de/evoila/cf/broker/controller/utils/JobProgressUtils.java
+++ core/src/main/java/de/evoila/cf/broker/controller/utils/JobProgressUtils.java
@@ -1,6 +1,7 @@
 package de.evoila.cf.broker.controller.utils;
 
 import de.evoila.cf.broker.model.JobProgressResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -9,15 +10,19 @@ import org.springframework.stereotype.Service;
 @Service
 public class JobProgressUtils {
 
-    @Value("${server.retry-after}")
-    private Integer retryAfter;
+    private int retryAfter;
 
     public ResponseEntity<JobProgressResponse> buildJobProgressResponseEntity(JobProgressResponse jobProgressResponse) {
         ResponseEntity.BodyBuilder builder = ResponseEntity.status(HttpStatus.OK);
-        if (retryAfter != null) {
+        if (retryAfter > 0) {
             builder.header("Retry-After", Integer.toString(retryAfter));
         }
 
         return builder.body(jobProgressResponse);
+    }
+
+    @Autowired
+    public void setRetryAfter(@Value("${server.retryAfter:0}") int retryAfter) {
+        this.retryAfter = retryAfter;
     }
 }
