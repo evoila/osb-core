1be04ccbf2cee4f0e627aaeb11a0b2bc16a8617d
MaxFuhrich <maxfuhrich@gmail.com>
2022-11-18 14:07:49 +0100

    feat: add setManifestMetadata in replaceParameters of MongoDBDeploymentManager.java debug: remove logs

--- core/src/main/java/de/evoila/cf/broker/controller/core/ServiceInstanceController.java
+++ core/src/main/java/de/evoila/cf/broker/controller/core/ServiceInstanceController.java
@@ -87,7 +87,6 @@ public class ServiceInstanceController extends BaseController {
         }
 
         checkMaintenanceInfo(request);
-        log.debug("Create, before createServiceInstance");
         ServiceInstanceOperationResponse response = deploymentService.createServiceInstance(serviceInstanceId, request);
 
         log.debug("ServiceInstance Creation Started: " + serviceInstanceId);
